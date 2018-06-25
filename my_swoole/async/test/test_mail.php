1a328095cd9f439db6a0af4aae37ce28e838984d
chinaprogramerhope <31737416+chinaprogramerhope@users.noreply.github.com>
2018-06-25 11:56:16 +0800

    #update

--- my_swoole/async/test/test_mail.php
+++ my_swoole/async/test/test_mail.php
@@ -1,28 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hanxiaolong
- * Date: 2018/6/15
- * Time: 10:58
- */
 require_once '../spl_autoload_register.php';
 
 $url = 'http://127.0.0.1:9401';
 $param = [
-    'port' => 25,
-    'user' => 'phphack@163.com',
-    'pass' => 'han888',
-    'host' => 'smtp.163.com',
-    'from' => 'phphack@163.com',
-    'to' => '18301805881@163.com',
-    'subject' => 'test_mail',
-    'body' => 'gagaga',
+'to' => '18301805881@163.com',
+'subject' => 'test_mail',
+'body' => 'gagaga',
 ];
 
 $params = [
-    'class_name' => 'Mail',
-    'func_name' => 'sendMail',
-    'param' => json_encode($param),
+'class_name' => 'svcMail',
+'func_name' => 'send_verify_mail',
+'param' => $param,
 ];
 
 Http::curl_post($url, $params);
