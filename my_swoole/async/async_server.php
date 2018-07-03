d88dd67a35a742f1bde37c86ae5f7a9f833f3d54
chinaprogramerhope <31737416+chinaprogramerhope@users.noreply.github.com>
2018-07-03 15:29:44 +0800

    #update

--- my_swoole/async/async_server.php
+++ my_swoole/async/async_server.php
@@ -27,22 +27,7 @@ $http_server->set([
 ]);
 
 $http_server->on('request', function (swoole_http_request $request, swoole_http_response $response) use ($http_server, $redis, $key_prefix) {
-    // 请求过滤
-    if ($request->server['path_info'] == '/favicon.info' || $request->server['request_uri'] == '/favicon.ico') { // todo
-        $response->end(); // todo return $response->end();
-    }
-
-    $task_id = isset($request->get['taskId']) ? $request->get['taskId'] : '';
-    if ($task_id != '') {
-        // 返回任务状态
-        $status = $redis->get($key_prefix . $task_id);
-        $response->end("task: $task_id; status: $status"); // todo return
-    }
-
-    $param = $request->post; // 此处处理request请求数据作为任务执行的数据, 根据需要修改
-    $task_id = $http_server->task($param);
-
-    // todo 错误和成功的返回在各个接口中返回; 并在这里得到各个接口的返回值并返回
+    // 返回值默认格式
     $error_ret_server = [
         "status" => 500,
         "data" => [],
@@ -58,34 +43,61 @@ $http_server->on('request', function (swoole_http_request $request, swoole_http_
         "data" => [],
         "msg" => ""
     ];
+
+    $task_id = isset($request->get['taskId']) ? $request->get['taskId'] : '';
+    if ($task_id != '') { // 返回任务状态
+        $task_status = $redis->get($key_prefix . $task_id);
+        $success_ret['task_status'] = $task_status;
+        $response->end(json_encode($success_ret));
+    }
+
+    $param = $request->post; // 此处处理request请求数据作为任务执行的数据, 根据需要修改
+
+    Log::info(__METHOD__ . ', ' . __LINE__ . ', param_type = ' . gettype($param) . ', data_content = ' . json_encode($param));
+
+    // 参数检查
+    if (!isset($param['class_name']) || !isset($param['func_name'])) {
+        Log::error(__METHOD__ . ' : ' . __LINE__ . ' invalid param, param = ' . json_encode($param));
+        $error_ret_client['msg'] = ' invalid param';
+        $response->end(json_encode($error_ret_client));
+    }
+    $param['func_param'] = isset($param['func_param']) ? $param['func_param'] : [];
+    if (!is_array($param['func_param'])) {
+        Log::error(__METHOD__ . ', ' . __LINE__ . ', invalid param, func_param is not array, func_param_type = '
+            . gettype($param['func_param']));
+        $error_ret_client['msg'] = ' invalid param';
+        $response->end(json_encode($error_ret_client));
+    }
+
+    $task_id = $http_server->task($param);
+
+    if ($task_id === false) {
+        Log::error(__METHOD__ . ', ' . __LINE__ . ', task fail');
+        $error_ret_server['msg'] = ' task fail in request';
+        $response->end(json_encode($success_ret));
+    }
+
     $success_ret['data']['task_id'] = $task_id;
     $response->end(json_encode($success_ret));
 });
 
 // 处理异步任务
 $http_server->on('task', function ($server, $task_id, $from_id, $data) use ($redis, $key_prefix) {
-    // 任务处理, 可以把处理结果和状态在redis里面实时更新, 便于获取任务状态
-    for ($i = 1; $i <= 3; ++$i) {
-        $redis->set($key_prefix . $task_id, $i);
-//        sleep(2);
-    }
-
-    Log::info(__METHOD__ . ' : ' . __LINE__ . ' data_type = ' . gettype($data) . ', data_content = ' . json_encode($data));
-
-    if (!isset($data['class_name']) || !isset($data['func_name'])) {
-        Log::error(__METHOD__ . ' : ' . __LINE__ . ' invalid param, param = ' . json_encode($data));
-        return ERROR_INVALID_PARAM;
-    }
-
-    $param = isset($data['param']) ? $data['param'] : [];
-    if (!is_array($param)) {
-        Log::error(__METHOD__ . ' : ' . __LINE__ . ' invalid param, param is not array, param_type = ' . gettype($param));
-        return ERROR_INVALID_PARAM;
-    }
+    $task_status = 'success'; // todo 更丰富的状态, 根据需求
 
     $class = new $data['class_name']();
     $func_name = $data['func_name'];
-    return $class->$func_name($param); // 必须有return, 否则不会调用onFinish
+    $error_code = $class->$func_name($data['param']); // 必须有return, 否则不会调用onFinish
+    if ($error_code !== ERROR_OK) {
+        Log::error(__METHOD__ . ', ' . __LINE__ . ' task fail in task');
+
+        $task_status = 'fail';
+    }
+
+    // 保存任务执行结果 (客户端根据需要来查询任务执行结果)   todo 主动通知客户端任务执行结果;
+    $redis->set($key_prefix . $task_id, $task_status);
+
+    return;
 });
 
 // 任务结束之后处理任务或者回调
